2ec80f32670b65dafaf7c7bdc5349a2f64ab01b7
ela <patriciaaela@gmail.com>
2024-08-27 20:43:07 +0700

    change top rated display

--- src/components/Home/TopRated.js
+++ src/components/Home/TopRated.js
@@ -5,7 +5,7 @@ import CardMovie from "../CardMovie";
 export default function TopRated({ movie }) {
   return (
     <div className="container top-rated">
-      <Row xs="2" sm="3" md="4" lg="5" xl="6" className="rows">
+      <Row xs="2" sm="3" md="4" lg="4" xl="5" className="rows">
         {movie.map((mov) => (
           <Col key={mov.id} className="d-flex justify-content-center mb-2">
             <CardMovie movie={mov} />
